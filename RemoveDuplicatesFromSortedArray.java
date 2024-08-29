1cb8d4e75eae49f690a73ef3b7c2e13ca047506e
katriinanow <62251467+katriinanow@users.noreply.github.com>
2024-08-29 22:15:11 +0200

    RemoveDuplicatesFromSortedArray challenge

---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,53 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class RemoveDuplicatesFromSortedArray {
+
+    static int[] readInput(Scanner scanner) {
+        String input = scanner.nextLine();
+        String[] stringNums = input.split(",\\s*");
+
+        List<Integer> numbersList = new ArrayList<>();
+        for (String strNum : stringNums) {
+            try {
+                numbersList.add(Integer.parseInt(strNum));
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number format: " + strNum);
+            }
+        }
+
+        int[] nums = numbersList.stream().mapToInt(i -> i).toArray();
+
+        return nums;
+    }
+
+    static void removeDuplicates(int[] nums) {
+        int j = 1;
+        if (nums.length > 0) {
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] != nums[j-1]) {
+                    j++;
+                    nums[j-1] = nums[i];
+                }
+            }
+            for (int i = j; i < nums.length; i++) {
+                nums[i] = -101;
+            }
+        }
+        System.out.print(j + ", ");
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] nums = readInput(scanner);
+        scanner.close();
+
+        removeDuplicates(nums);
+
+    }
+}
